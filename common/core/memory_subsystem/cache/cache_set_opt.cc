1de92510119538bd8bffaf0b54d7109e70e1e77f
Yanjie Song <songyj@shanghaitech.edu.cn>
2020-11-07 20:23:01 +0800

    Optimal policy finished.

--- common/core/memory_subsystem/cache/cache_set_opt.cc
+++ common/core/memory_subsystem/cache/cache_set_opt.cc
@@ -7,9 +7,7 @@ TraceVector trace_vector;
 
 CacheSetOPT::CacheSetOPT (CacheBase::cache_t cache_type, UInt32 associativity, UInt32 blocksize)
 : CacheSet (cache_type, associativity, blocksize)
-{
-  m_replacement_index = m_associativity - 1;
-}
+{}
 
 CacheSetOPT::~CacheSetOPT ()
 {}
@@ -17,17 +15,52 @@ CacheSetOPT::~CacheSetOPT ()
 UInt32
 CacheSetOPT::getReplacementIndex (CacheCntlr *cntlr)
 {
-  UInt32 curr_replacement_index = m_replacement_index;
-  m_replacement_index = (m_replacement_index == 0) ? (m_associativity - 1) : (m_replacement_index - 1);
+  // First try to find an invalid block
+  for (UInt32 i = 0; i < m_associativity; i++)
+    {
+      if (!m_cache_block_info_array[i]->isValid ())
+        {
+          return i;
+        }
+    }
 
-  if (!isValidReplacement (m_replacement_index))
-    return getReplacementIndex(cntlr);
+  // Evict the block with farthest access time.
+  int max_distance = 0;
+  UInt32 index = m_associativity;
+  for (UInt32 i = 0; i < m_associativity; i++)
+    {
+      // Skip the current index if not available
+      if (!isValidReplacement (i))
+        continue;
+
+      IntPtr tag = m_cache_block_info_array[i]->getTag ();
+
+      // There is no access afterwards
+      int current_distance = trace_vector.nextAccess (tag);
+      if (current_distance == -1)
+        {
+          return i;
+        }
+      // A block with farther access time
+      else if (current_distance > max_distance)
+        {
+          max_distance = current_distance;
+          index = i;
+        }
+    }
+
+  // Check whether we found one
+  if (index == m_associativity)
+    {
+      LOG_PRINT_ERROR ("Cannot find index.");
+      return 0;
+    }
   else
-    return curr_replacement_index;
+    {
+      return index;
+    }
 }
 
 void
-CacheSetOPT::updateReplacementIndex (UInt32 accessed_index)
-{
-  return;
-}
+CacheSetOPT::updateReplacementIndex (UInt32 accessed_index __attribute__ ((unused)))
+{}
